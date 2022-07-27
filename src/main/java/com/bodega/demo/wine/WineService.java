df012d67ec9fde97337b7c671357e4c1cbf92357
Joselito <jeuo@gft.com>
2022-07-27 18:30:38 +0200

    Implementar seguridad y autorizacion por roles

--- src/main/java/com/bodega/demo/wine/WineService.java
+++ src/main/java/com/bodega/demo/wine/WineService.java
@@ -13,46 +13,44 @@ public class WineService {
 
 	@Autowired
 	private WineRepository wineRepository;
-	
-	public List<Wine> getAll(){
+
+	public List<Wine> getAll() {
 		return wineRepository.findAll();
 	}
-	
-	public Wine getOne(int id){
+
+	public Wine getOne(int id) {
 		return wineRepository.findById(id).orElse(null);
 	}
-	
-	public void deletebyId(int id){
-		 wineRepository.deleteById(id);
+
+	public void deletebyId(int id) {
+		wineRepository.deleteById(id);
 	}
-	
-	
+
 	public Wine save(Wine wine) {
 		return wineRepository.save(wine);
 	}
-	
-	
-	public List<Wine> getBest(Pageable limit){
+
+	public List<Wine> getBest(Pageable limit) {
 		return wineRepository.findAllOrderByRatingDesc(limit);
 	}
-	
-	public List<Wine> getMostExpensive(Pageable limit){
+
+	public List<Wine> getMostExpensive(Pageable limit) {
 		return wineRepository.findAllOrderByPriceDesc(limit);
 	}
-	
-	public List<Wine> getBestBangForTheBuck(Pageable limit){
+
+	public List<Wine> getBestBangForTheBuck(Pageable limit) {
 		return wineRepository.findBestBangForTheBuck(limit);
 	}
-	
-	public HashMap<String, List<Wine>> getVintageList(Pageable limit){
+
+	public HashMap<String, List<Wine>> getVintageList(Pageable limit) {
 		List<String> yearsWithBestRatedWine = wineRepository.findYearsWithBestRatedWines(limit);
 		HashMap<String, List<Wine>> topWines = new HashMap<>();
-		
+
 		for (String year : yearsWithBestRatedWine) {
 			List<Wine> wines = wineRepository.findByYear(year);
 			topWines.put(year, wines);
 		}
-		
+
 		return topWines;
 	}
 }
